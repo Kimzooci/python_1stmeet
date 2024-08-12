3aa6f97cad593d58f3c78200b30730cfabacd6a5
kimzooci <kjhvkwn@gmail.com>
2024-08-12 15:56:23 +0900

    힘들다

--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -1,33 +1,19 @@
 const SLOTS_PER_REEL = 14;
-const REEL_RADIUS = Math.round(149 * Math.tan( Math.PI / 12) / Math.tan( Math.PI / 14));
+const REEL_RADIUS = Math.round(149 * Math.tan(Math.PI / 12) / Math.tan(Math.PI / 14));
 
 function createSlots(ring) {
-    var slotAngle = 360 / SLOTS_PER_REEL;
-    var seed = getSeed();
-    for (var i = 0; i < SLOTS_PER_REEL; i++) {
-        var slot = document.createElement('div');
+    const slotAngle = 360 / SLOTS_PER_REEL;
+    const seed = getSeed();
+
+    for (let i = 0; i < SLOTS_PER_REEL; i++) {
+        const slot = document.createElement('div');
         slot.className = 'slot';
-        var transform = 'rotateX(' + (slotAngle * i) + 'deg) translateZ(' + REEL_RADIUS + 'px)';
+        const transform = 'rotateX(' + (slotAngle * i) + 'deg) translateZ(' + REEL_RADIUS + 'px)';
         slot.style.transform = transform;
-        var slotContent = '';
-        switch((seed + i) % 14) {
-            case 0: slotContent = 'ㄱ'; break;
-            case 1: slotContent = 'ㄴ'; break;
-            case 2: slotContent = 'ㄷ'; break;
-            case 3: slotContent = 'ㄹ'; break;
-            case 4: slotContent = 'ㅁ'; break;
-            case 5: slotContent = 'ㅂ'; break;
-            case 6: slotContent = 'ㅅ'; break;
-            case 7: slotContent = 'ㅇ'; break;
-            case 8: slotContent = 'ㅈ'; break;
-            case 9: slotContent = 'ㅊ'; break;
-            case 10: slotContent = 'ㅋ'; break;
-            case 11: slotContent = 'ㅌ'; break;
-            case 12: slotContent = 'ㅍ'; break;
-            case 13: slotContent = 'ㅎ'; break;
-        }
+
+        const slotContent = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'][(getSeed() + i) % 14];
         slot.innerHTML = '<p>' + slotContent + '</p>';
-        ring.appendChild(slot);
+        ring.append(slot);
     }
 }
 
@@ -36,22 +22,23 @@ function getSeed() {
 }
 
 function spin(timer) {
-    for (var i = 1; i < 3; i++) {
-        var oldClass = document.querySelector('#ring' + i).className;
-        var seed = getSeed();
-        while (oldClass === 'ring spin-' + seed) {
+    for (let i = 1; i < 3; i++) {
+        const oldClass = document.getElementById('ring' + i).className;
+        let seed = getSeed();
+        while (oldClass.trim() === 'ring spin-' + seed) {
             seed = getSeed();
         }
-        var ring = document.querySelector('#ring' + i);
-        ring.style.animation = 'back-spin 1s, spin-' + seed + ' ' + (timer + i * 0.5) + 's';
-        ring.className = 'ring spin-' + seed;
+        document.getElementById('ring' + i)
+            .style.animation = 'back-spin 1s, spin-' + seed + ' ' + (timer + i * 0.5) + 's';
+        document.getElementById('ring' + i).className = 'ring spin-' + seed;
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    createSlots(document.querySelector('#ring1'));
-    createSlots(document.querySelector('#ring2'));
-    document.querySelector('.go').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', () => {
+    createSlots(document.getElementById('ring1'));
+    createSlots(document.getElementById('ring2'));
+
+    document.querySelector('.go').addEventListener('click', () => {
         spin(2);
     });
 });
